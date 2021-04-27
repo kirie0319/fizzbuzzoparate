0b890bd74b4e731510b20c1c7000cbb0add5ffae
kirie0319 <matsueda0407@icloud.com>
2021-04-27 21:59:14 +0900

    タスク追加機能完成4/27

--- js/main.js
+++ js/main.js
@@ -1,36 +1,57 @@
 'use strict';
 {
-  const addBtn = document.getElementById('add_btn');
   const todos = [];
-  
+  const textAdd = document.getElementById('text_add');
+  const task = {
+    content: '',
+    status: '作業中'
+  };
 
-  addBtn.addEventListener('click', () =>{
-    const textAdd = document.getElementById('text_add');
-    const value = text_add.value;
-    const addId = document.getElementById('add_id');
-    const addStatus = document.getElementById('add_status');
-    const addComment = document.getElementById('add_comment');
-    const li = document.createElement('li');
-    const idLi = document.createElement('li');
-    const div =  document.createElement('div');
-    const deleteBtn = document.createElement('a');
-    const statusBtn = document.createElement('a');
-    todos.push(value);
-    deleteBtn.textContent = '削除';
-    deleteBtn.classList.add('btn');
-    statusBtn.textContent = '作業中';
+  const addBtn = document.getElementById('add_btn');
+  const add = document.getElementById('add');
+
+  function addTask() {
+    const content = textAdd.value;
+    task.content = content
+    todos.push(task)
+    textAdd.value = ''
+  }
+  function displyTask(todos) {
+    const tr =  document.createElement('tr');
+    const idTd = document.createElement('td');
+    const td = document.createElement('td');
+    todos.forEach(task => {
+      //IDを添付する処理
+      for (let i = 0; i < todos.length; i++) {
+        idTd.textContent = `${i}`;
+      }
+      //タスクの内容を添付する処理
+      td.textContent = task.content
+      tr.innerHTML = '';
+      //HTML部に表示する処理
+      tr.appendChild(idTd);
+      tr.appendChild(td);
+      //タスクボタンに状態を添付する処理
+      createStatus(task, tr)
+      //削除ボタンに削除と添付する処理
+      createDelete(tr)
+      add.appendChild(tr);
+    })
+  }
+  function createStatus(task, tr) {
+    const statusBtn = document.createElement('button');
     statusBtn.classList.add('btn');
-    li.textContent = value;
-    for (let i = 0; i < todos.length; i++) {
-      idLi.textContent = `${i}`;
-    }
-    addId.appendChild(idLi);
-    addComment.appendChild(li);
-    textAdd.value = '';
-    div.appendChild(statusBtn);
-    div.appendChild(deleteBtn);
-    addStatus.appendChild(div);
-    
-
+    statusBtn.textContent = task.status;
+    tr.appendChild(statusBtn);
+  }
+  function createDelete(tr) {
+    const deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('btn');
+    deleteBtn.textContent = '削除';
+    tr.appendChild(deleteBtn);
+  }
+  addBtn.addEventListener('click', () =>{
+    addTask();
+    displyTask(todos);
   });
 };
