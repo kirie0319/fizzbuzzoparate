a5c9e06cddd689b2327af76a2f218b140dab592f
kirie0319 <matsueda0407@icloud.com>
2021-05-17 13:56:25 +0900

    タスク表示/非表示機能5/17

--- js/main.js
+++ js/main.js
@@ -4,15 +4,19 @@
   const textAdd = document.getElementById('text_add');
   const task = {
     content: '',
-    status: '作業中'
+    status: ''
   };
 
   const addBtn = document.getElementById('add_btn');
   const add = document.getElementById('add');
 
+  const overall = document.getElementById('all');
+  const workon = document.getElementById('workon');
+  const done = document.getElementById('done');
+
   function addTask() {
     const content = textAdd.value;
-    task.content = content
+    task.content = content;
     todos.push({
       content,
       status: '作業中'
@@ -22,7 +26,7 @@
 
   function displayTask(todos) {
     add.innerHTML = '';
-    todos.forEach((task, index) => {
+    todos.forEach((todo, index) => {
       //IDを添付する処理
       const tr =  document.createElement('tr');
       const idTd = document.createElement('td');
@@ -32,37 +36,50 @@
         idTd.textContent = index;
       }
       //タスクの内容を添付する処理
-      td.textContent = task.content
+      td.textContent = todo.content
       tr.innerHTML = '';
       //HTML部に表示する処理
       tr.appendChild(idTd);
       tr.appendChild(td);
+      tr.classList.add('focus');
+      if (todo.status === '完了') {
+        tr.classList.remove('focus');
+        tr.classList.add('complete');
+      }
       //タスクボタンに状態を添付する処理
-      createStatus(task, tr)
+      createStatus(todo, tr)
       //削除ボタンに削除と添付する処理
       createDelete(tr, index)
       add.appendChild(tr);
     })
   }
 
-  function createStatus(task, tr) {
+  function createStatus(todo, tr) {
     const statusBtn = document.createElement('button');
     statusBtn.classList.add('btn');
-    statusBtn.textContent = task.status;
+    statusBtn.textContent = todo.status;
     tr.appendChild(statusBtn);
-    chageStatus(statusBtn);
+    changeStatus(statusBtn, todo, tr);
   }
 
-  const chageStatus = function (statusBtn) {
+  const changeStatus = function (statusBtn, todo, tr) {
     statusBtn.addEventListener('click', () => {
-      if (task.status === '作業中') {
-        console.log(task.status);
-        task.status = '完了';
-        statusBtn.textContent = task.status;
+      if (todo.status === '作業中') {
+        todo.status = '完了';
+        statusBtn.textContent = todo.status;
+        tr.classList.add('complete');
+        tr.classList.remove('focus');
+        if (workon.checked) {
+          tr.classList.add('unviewed');
+        }
       } else {
-        console.log(task.status);
-        task.status = '作業中'
-        statusBtn.textContent = task.status;
+        todo.status = '作業中'
+        statusBtn.textContent = todo.status;
+        tr.classList.remove('complete');
+        tr.classList.add('focus');
+        if (done.checked) {
+          tr.classList.add('unviewed');
+        }
       }
     });
   }
@@ -80,11 +97,88 @@
       add.removeChild(tr);
       todos.splice(index, 1);
       displayTask(todos);
+      if (workon.checked) {
+        const completes = document.querySelectorAll('.complete');
+        completes.forEach((complete) => {
+          if (todos.status !== '作業中') {
+            complete.classList.add('unviewed');
+          }
+        });
+      }
+      if (done.checked) {
+        const focuses = document.querySelectorAll('.focus');
+        focuses.forEach((focus) => {
+          if (todos.status !== '完了') {
+            focus.classList.add('unviewed');
+          }
+        });
+      }
     });
   }
 
   addBtn.addEventListener('click', () =>{
     addTask();
     displayTask(todos);
+    if (workon.checked) {
+      const completes = document.querySelectorAll('.complete');
+      completes.forEach((complete) => {
+        if (todos.status !== '作業中') {
+          complete.classList.add('unviewed');
+        }
+      });
+    }
+    if (done.checked) {
+      const focuses = document.querySelectorAll('.focus');
+      focuses.forEach((focus) => {
+        if (todos.status !== '完了') {
+          focus.classList.add('unviewed');
+        }
+      });
+    }
+  });
+
+  overall.addEventListener('click', () => {
+    const completes = document.querySelectorAll('.complete');
+    completes.forEach((complete) => {
+      if (todos.status !== '作業中') {
+        complete.classList.remove('unviewed');
+      }
+    });
+    const focuses = document.querySelectorAll('.focus');
+    focuses.forEach((focus) => {
+      if (todos.status !== '完了') {
+        focus.classList.remove('unviewed');
+      }
+    });
+  });
+
+  workon.addEventListener('click', () => {
+    const completes = document.querySelectorAll('.complete');
+    completes.forEach((complete) => {
+      if (todos.status !== '作業中') {
+        complete.classList.add('unviewed');
+      }
+    });
+    const focuses = document.querySelectorAll('.focus');
+    focuses.forEach((focus) => {
+      if (todos.status !== '完了') {
+        focus.classList.remove('unviewed');
+      }
+    });
+  });
+
+  done.addEventListener('click', () => {
+    const focuses = document.querySelectorAll('.focus');
+    focuses.forEach((focus) => {
+      if (todos.status !== '完了') {
+        focus.classList.add('unviewed');
+      }
+    });
+    const completes = document.querySelectorAll('.complete');
+    completes.forEach((complete) => {
+      if (todos.status !== '作業中') {
+        complete.classList.remove('unviewed');
+      }
+    });
   });
 };
